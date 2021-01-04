dd60108223d332311c3e9a6d7f0c432be9bfa881
CrgioPeca88 <crgiopeca88@gmail.com>
2021-01-04 17:55:12 -0500

    [FEATURE] Css responsive design and animations test added - css-responsive.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { ScheduleComponent } from './schedule/schedule.component';
 import { CssTestComponent } from './css-test/css-test.component';
+import { CssResponsiveTestComponent } from './css-responsive/css-responsive.component';
 import { AppService } from './app.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     CssTestComponent,
+    CssResponsiveTestComponent,
     RegistrationComponent,
     ScheduleComponent
   ],
---
+++ src/app/css-responsive/css-responsive.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'css-responsive',
+  templateUrl: './css-responsive.component.html',
+  styleUrls: ['./css-responsive.component.css']
+})
+export class CssResponsiveTestComponent {
+
+  constructor() {}
+
+}
