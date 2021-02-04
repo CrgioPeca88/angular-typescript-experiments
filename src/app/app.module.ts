ea725a2547cc69d09788557ea9c86a2208267bf1
CrgioPeca88 <crgiopeca88@gmail.com>
2021-02-04 14:23:35 -0500

    [UPGRADE] Updating angular version to 5, part 3.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { BrowserModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
